09cc8afb290e3d5239d5abda72ba6c588bbfd696
djbrowlite <35111445+djbrowlite@users.noreply.github.com>
2025-07-18 04:39:11 -0400

    Create setup_env.bat

---
+++ setup_env.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Creando entorno virtual...
+python -m venv .venv
+
+echo Activando entorno virtual...
+call .venv\Scripts\activate.bat
+
+echo Instalando dependencias...
+pip install pandas requests oandapyV20
+
+echo Entorno listo. Puedes ejecutar el bot con: python main.py
+pause
