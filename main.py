a645f93047cf19c48c9cc014b9d550ba3ed2b537
djbrowlite <35111445+djbrowlite@users.noreply.github.com>
2025-07-18 07:02:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import os
+
+# Inicialización de carpeta y archivo de historial
+os.makedirs("logs", exist_ok=True)
+csv_path = "logs/operaciones.csv"
+if not os.path.exists(csv_path) or os.path.getsize(csv_path) == 0:
+    with open(csv_path, mode="w", newline="") as file:
+        file.write("Fecha,Symbol,Precio Entrada,Stop Loss,Take Profit,Lotaje,Modo,Resultado\n")
+
 import tkinter as tk
 from gui import TradingBotGUI
 
